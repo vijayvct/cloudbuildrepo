01379e14659db3302b5cd3657027b8dd39fa5ca4
vijay.v1318@gmail.com <vijay.v1318@gmail.com>
2024-07-11 08:00:04 +0000

    file modified

--- obj/Debug/net6.0/cloudbuilddemo.AssemblyInfo.cs
+++ obj/Debug/net6.0/cloudbuilddemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloudbuilddemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18e084cb34d156acad28f996879230140102a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloudbuilddemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cloudbuilddemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
