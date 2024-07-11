bb530cc8e330cb69732aabf9461f573b28c0de69
vijay.v1318@gmail.com <vijay.v1318@gmail.com>
2024-07-11 11:42:24 +0000

    Dockerfile updated

--- obj/Release/net6.0/cloudbuilddemo.AssemblyInfo.cs
+++ obj/Release/net6.0/cloudbuilddemo.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloudbuilddemo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa37dbfbb28a6515d056679c847b45946189d2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloudbuilddemo")]
